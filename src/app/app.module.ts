58294a0c6abe62b760f4b01f976e78d46db8163b
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-06-15 11:17:39 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmulatedEncapsulationComponent } from './emulated-encapsulation.component';
+import { NoEncapsulationComponent } from './no-encapsulation.component';
+import { ShadowDomEncapsulationComponent } from './shadow-dom-encapsulation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoEncapsulationComponent,
+    ShadowDomEncapsulationComponent,
+    EmulatedEncapsulationComponent
   ],
   imports: [
     BrowserModule
